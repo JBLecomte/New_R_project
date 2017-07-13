26f56455676256f5937ef7b954f935cdfb105f7f
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2017-07-13 18:24:39 +0200

    Initial commit

---
+++ Main.R
@@ -0,0 +1,7 @@
+###########################################
+## Main R script, which runs the analysis
+###########################################
+rm(list = ls(all = TRUE))
+
+### Source useful package and functions
+source('R/Fun/Boot.R')
---
+++ README.md
@@ -0,0 +1,9 @@
+# Template for an R project
+Contact info
+----------------------------
+Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
+
+
+
+
+
