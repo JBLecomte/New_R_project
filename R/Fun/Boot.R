ce9e4b814feef8c7801c5109d2cd6483992e95e0
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2017-07-13 18:35:57 +0200

    useful R fct and boot.R script

---
+++ R/Fun/Boot.R
@@ -0,0 +1,67 @@
+##########################################################
+## First R file source, load useful packages and function
+##########################################################
+options(install.packages.check.source = "no")
+
+#### Load required package, and install it if not
+list.of.packages <- c('ggplot2', 'grid', 'scales', 'gridExtra', 'latex2exp', 'GGally', 'ggmcmc', 'RColorBrewer', # plot
+                      'ggmap', 'maps', 'mapdata', 'maptools', # ploting maps
+                      'tidyverse', 'dplyr', 'tidyr', # data manipulation
+                      'knitr', 'rmarkdown', 'captioner', 'bookdown', 'pander', # dynamic document
+                      'sp', 'rgdal', 'spatstat', 'raster', 'fields', 'rasterVis', 'rgl' #,  # spatial packages
+                      #'gpclib',
+                      #'nimble', 'coda',
+                      #'rjags', 'rbugs', 'R2OpenBUGS', # BUGS packages
+                      #'dclone',
+                      #'snow' # paralell computing
+) 
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages)) install.packages(new.packages, type = 'binary')
+lapply(list.of.packages, require, character.only = TRUE)
+
+##########################
+### ggplot2 functions
+##########################
+### Sober plot theme
+source('R/Fun/ggplot2/SimpleTheme_ggplot2.R')
+
+### Function to produce customs boxplot
+source('R/Fun/ggplot2/ggplot2_summary_fct_boxplot.R')
+
+### Palette
+source('R/Fun/ggplot2/my_palette.R')
+
+### Palette
+source('R/Fun/ggplot2/save_plot.R')
+
+### Extract legend from a plot
+source('R/Fun/ggplot2/extract_plot_legend.R')
+
+##########################
+### Other functions
+##########################
+### Load the utilities functions and control file variables
+source('R/Fun/Utils/utilities.R')
+
+### Unpack the object stored in a list
+source('R/Fun/Utils/unpack.list.R')
+
+### Compute the 25% quantile
+source('R/Fun/Utils/quantile25.R')
+source('R/Fun/Utils/quantile75.R')
+
+### Standardization of a variable
+source('R/Fun/Utils/Cov_Std.R')
+
+### Cutting a variable
+source('R/Fun/Utils/cut_continuous_variable.R')
+
+### List objects in R + print their size
+source('R/Fun/Utils/list_objects_and_size.R')
+
+##########################
+### Project's functions
+##########################
+source('R/Fun/Project/RBlocksv1.1.r')
---
+++ R/Fun/ggplot2/save_plot.R
@@ -0,0 +1,70 @@
+save_plot <- function(plot,
+                      file_name,
+                      path,
+                      output = '',
+                      scale = 1,
+                      width = NULL,
+                      height = NULL,
+                      device = 'pdf',
+                      Save = TRUE){
+  if(SavePlots){
+    
+    if(file.exists(paste0(path, file_name)) & EraseCoda == FALSE){
+      warning('plot already saved')
+    }else{
+      
+      if(output == 'Beamer'){
+        if(is.null(width)){
+          width <- 12  
+        }
+        
+        if(is.null(height)){
+          height <- 8
+        }
+        
+        units <- 'cm'
+        scale <- 1.5
+        
+        ggsave(plot = plot,
+               filename = paste0(file_name, '.' ,device),
+               device = device,
+               path = path,
+               scale = scale,
+               dpi = 600,
+               width = width,
+               height = height,
+               units = units,
+               bg = "transparent")
+      }
+      
+      if(output == 'Beamer/2'){
+        width <- 10.8/2
+        height <- 8.38/2
+        units <- 'cm'
+        scale <- 1.5
+        
+        ggsave(plot = plot,
+               filename = paste0(file_name, '.' ,device),
+               device = device,
+               path = path,
+               scale = scale,
+               dpi = 600,
+               width = width,
+               height = height,
+               units = units,
+               bg = "transparent")
+      }
+      
+      
+      if(output == ''){
+        ggsave(plot = plot,
+               filename = paste0(file_name, '.' ,device),
+               device = device,
+               path = path,
+               scale = scale,
+               dpi = 600,
+               bg = "transparent")
+      }
+    }
+  }
+}
