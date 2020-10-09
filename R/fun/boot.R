bc899021f5af84aa67d763c4a565c10e27e8e204
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2020-10-09 10:45:11 +0200

    update packages and automatically load project function with purrr::map

--- R/fun/boot.R
+++ R/fun/boot.R
@@ -1,71 +1,85 @@
-##########################################################
-## First R file source, load useful packages and function
-##########################################################
-options(install.packages.check.source = "no")
+################################################################################
+### First R file source, load useful packages and function
+################################################################################
 
+options(install.packages.check.source = "yes")
 
+###-----------------------------------------------------------------------------
 #### Load required package, and install it if not
-list.of.packages <- c('ggplot2', 'grid', 'scales', 'gridExtra', 'latex2exp', 'GGally', 'ggmcmc', 'RColorBrewer', # plot
-                      'ggmap', 'maps', 'mapdata', 'maptools', # ploting maps
-                      'dplyr', 'tidyr', # data manipulation
-                      'knitr', 'rmarkdown', 'captioner', 'bookdown', 'pander', # dynamic document
-                      'sp', 'rgdal', 'spatstat',  # spatial packages
-                      # 'gpclib',
-                      'nimble', 'coda',
-                      #'rjags', 'rbugs', 'R2OpenBUGS', # BUGS packages
-                      #'dclone',
-                      'parallel' # paralell computing
-) 
+list_of_packages <- c( "tidyverse", "here", "lubridate", "remotes",
+                       # plot
+                       "grid", "scales", "gridExtra", "latex2exp", "GGally", "ggthemes", "gghighlight",
+                       # plotting maps
+                       "maps", "mapdata", "maptools",
+                       # dynamic document
+                       "knitr", "rmarkdown", "captioner", "bookdown", "pander", "flextable", 
+                       # spatial packages
+                       "sf", "ggsn",
+                       "JBUtilities") 
 
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
-if(length(new.packages)) install.packages(new.packages, type = 'binary')
-lapply(list.of.packages, require, character.only = TRUE)
+remotes::install_github('JBLecomte/JBUtilities', force = FALSE)
 
-##########################
+JBUtilities::InstallPackages(list_of_packages)
+lapply(list_of_packages, library, character.only = TRUE)
+
+
+###-----------------------------------------------------------------------------
+### Load control file
+source(here("R/load_control_file.R"))
+
+###-----------------------------------------------------------------------------
+### Load control file
+source(here("R/fun/make_path.R"))
+
+###-----------------------------------------------------------------------------
 ### ggplot2 functions
-##########################
+###-----------------------------------------------------------------------------
+
 ### Sober plot theme
-source('R/Fun/ggplot2/SimpleTheme_ggplot2.R')
+source(here("R/Fun/ggplot2/SimpleTheme_ggplot2.R"))
 
 ### Function to produce customs boxplot
-source('R/Fun/ggplot2/ggplot2_summary_fct_boxplot.R')
+source(here("R/Fun/ggplot2/ggplot2_summary_fct_boxplot.R"))
 
 ### Palette
-source('R/Fun/ggplot2/my_palette.R')
+source(here("R/Fun/ggplot2/my_palette.R"))
 
 ### Palette
-source('R/Fun/ggplot2/save_plot.R')
+source(here("R/Fun/ggplot2/save_plot.R"))
 
 ### Extract legend from a plot
-source('R/Fun/ggplot2/extract_plot_legend.R')
+source(here("R/Fun/ggplot2/extract_plot_legend.R"))
 
 ### Extract legend from a plot
-source('R/Fun/ggplot2/print.plot.R')
+source(here("R/Fun/ggplot2/print.plot.R"))
 
-##########################
+###-----------------------------------------------------------------------------
 ### Other functions
-##########################
+###-----------------------------------------------------------------------------
+
 ### Load the utilities functions and control file variables
-source('R/Fun/Utils/utilities.R')
+source(here("R/Fun/Utils/utilities.R"))
 
 ### Unpack the object stored in a list
-source('R/Fun/Utils/unpack.list.R')
+source(here("R/Fun/Utils/unpack.list.R"))
 
 ### Compute the 25% quantile
-source('R/Fun/Utils/quantile25.R')
-source('R/Fun/Utils/quantile75.R')
+source(here("R/Fun/Utils/quantile25.R"))
+source(here("R/Fun/Utils/quantile75.R"))
 
 ### Standardization of a variable
-source('R/Fun/Utils/Cov_Std.R')
+source(here("R/Fun/Utils/Cov_Std.R"))
 
 ### Cutting a variable
-source('R/Fun/Utils/cut_continuous_variable.R')
+source(here("R/Fun/Utils/cut_continuous_variable.R"))
 
 ### List objects in R + print their size
-source('R/Fun/Utils/list_objects_and_size.R')
+source(here("R/Fun/Utils/list_objects_and_size.R"))
 
-##########################
+###-----------------------------------------------------------------------------
 ### Project's functions
-##########################
+###-----------------------------------------------------------------------------
 
+invisible(purrr::map(paste0(c("R/fun/project/"),
+                            list.files(c("R/fun/project"), "*.R$")), source))
